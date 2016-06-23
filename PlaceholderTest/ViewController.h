9587c59c4a1fd80a7c244ff8430af70248b7e15a
BJTU-HT <bjtu-ht@163.com>
2016-06-23 23:02:46 +0800

    Initial Commit

---
+++ PlaceholderTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlaceholderTest
+//
+//  Created by tom555cat on 16/6/23.
+//  Copyright © 2016年 Hello World Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlaceholderTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlaceholderTest
+//
+//  Created by tom555cat on 16/6/23.
+//  Copyright © 2016年 Hello World Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
